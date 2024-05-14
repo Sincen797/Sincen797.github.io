1ce953461b0b7fbb43c36a14c364cab904dbb3b3
Sincen797 <157381511+Sincen797@users.noreply.github.com>
2024-05-14 12:45:22 +0000

    Circularity comp

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -28,7 +28,7 @@ var circles = []; // variable to store all circles in one Array
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-            physikz.addRandomVelocity(circle, canvas, 2.5, 2.5);
+            physikz.addRandomVelocity(circle, canvas, 5,5);
             view.addChild(circle);
             circles.push(circle);
         }
@@ -36,7 +36,7 @@ var circles = []; // variable to store all circles in one Array
 
         // TODO 3 / 7 : Call the drawCircle() function 
        
-        for (var loopsCompleted = 0; loopsCompleted < 10; loopsCompleted++) {
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
                     drawCircle(); 
         }
        
@@ -52,26 +52,21 @@ var circles = []; // variable to store all circles in one Array
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
+            for (var i = 0; i < circles.length; i++) {
+            physikz.updatePosition(circles[i]);
+            }
+    
           
 
 
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(circles[0]);
-            game.checkCirclePosition(circles[1]);
-            game.checkCirclePosition(circles[2]);
-            game.checkCirclePosition(circles[3]);
-            game.checkCirclePosition(circles[4]);
+            
 
             // TODO 9 : Iterate over the array
             for (var i = 0; i < circles.length; i++){
-                physikz.updatePosition(circles[i]);
+                physikz.updatePosition(circles[i])
                 game.checkCirclePosition(circles[i]);
                }
      
